5d119105d6b95ca807f349a698f439d9d0e90f16
继盛 <xuye.qin@alibaba-inc.com>
2015-11-30 13:40:06 +0800

    add requirements.txt to manifest

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ if os.path.exists('README.rst'):
         long_description = f.read()
 
 setup(name='pyodps',
-      version='0.2.4',
+      version='0.2.5',
       description='ODPS Python SDK',
       long_description=long_description,
       author='Wu Wei',
