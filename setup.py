a589453c9cecb12e158852e7c060d913649227f6
继盛 <xuye.qin@alibaba-inc.com>
2016-02-19 12:02:17 +0800

    upgrade version

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ if os.path.exists('README.rst'):
         long_description = f.read()
 
 setup(name='pyodps',
-      version='0.3.9',
+      version='0.3.10',
       description='ODPS Python SDK',
       long_description=long_description,
       author='Wu Wei',
