72ba6ecc2e70d50a581385fffb4fedd1950ed285
继盛 <xuye.qin@alibaba-inc.com>
2017-01-25 10:13:37 +0800

    upgrade to 0.6.9

--- odps/_version.py
+++ odps/_version.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-version_info = (0, 6, 8)
+version_info = (0, 6, 9)
 __version__ = '.'.join(map(str, version_info[:3])) + ''.join(version_info[3:])
