12275347644cddd142dd3f485fca45eef8e16a19
wenjun.swj <wenjun.swj@alibaba-inc.com>
2017-11-10 17:19:35 +0800

    upgrade to 0.7.12

---
+++ odps/lib/ext_types.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+# Copyright 1999-2017 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+import re
+
+if sys.version_info[0] <= 2:
+    string_types = basestring
+else:
+    string_types = str
+
+_MD_PATTERNS = [
+    re.compile(r'^(?P<month>(\d+|-\d+))$'),
+    re.compile(r'^(?P<year>(\d+|-\d+))\s*-\s*(?P<month>(\d+|-\d+))$'),
+    re.compile(r'^(?P<year>(\d+|-\d+))\s*(years|year)\s*(?P<month>(\d+|-\d+))\s*(months|month)$'),
+    re.compile(r'^(?P<year>(\d+|-\d+))\s*(years|year)$'),
+    re.compile(r'^(?P<month>(\d+|-\d+))\s*(months|month)$'),
+]
+
+
+class Monthdelta(object):
+    __slots__ = '_total_months',
+
+    def __init__(self, years=None, months=None):
+        if isinstance(years, string_types):
+            years, months = self._parse(years)
+        elif months is None:
+            years, months = months, years
+        years = years or 0
+        months = months or 0
+        self._total_months = int(years * 12 + months)
+
+    @classmethod
+    def _parse(cls, s):
+        s = s.strip()
+        for p in _MD_PATTERNS:
+            match = p.match(s)
+            if not match:
+                continue
+            match = match.groupdict()
+            return int(match.get('year', 0)), int(match.get('month', 0))
+        raise ValueError("Cannot parse '%s' for %s." % (s, cls.__name__))
+
+    def total_months(self):
+        return self._total_months
+
+    @property
+    def months(self):
+        return self._total_months % 12
+
+    @property
+    def years(self):
+        return self._total_months // 12
+
+    def __getstate__(self):
+        return dict(_total_months=self._total_months)
+
+    def __setstate__(self, state):
+        self._total_months = state['_total_months']
+
+    def __int__(self):
+        return self.total_months()
+
+    if sys.version_info[0] <= 2:
+        def __long__(self):
+            return self.total_months()
+
+    def __str__(self):
+        years = self.years
+        months = self.months
+        m = lambda v: 's' if abs(v) > 1 else ''
+        if years == 0 and months == 0:
+            return '0'
+        elif years == 0:
+            return '%d month%s' % (months, m(months))
+        elif months == 0:
+            return '%d year%s' % (years, m(years))
+        else:
+            return '%d year%s %d month%s' % (years, m(years), months, m(months))
+
+    def __repr__(self):
+        return "%s('%s')" % (type(self).__name__, str(self))
+
+    def __eq__(self, other):
+        if not isinstance(other, Monthdelta):
+            return False
+        return self._total_months == other._total_months
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __gt__(self, other):
+        try:
+            return self._total_months > other._total_months
+        except AttributeError:
+            raise TypeError("unsupported operand type(s) for >: '%s' and '%s'" %
+                            (type(self).__name__, type(other).__name__))
+
+    def __ge__(self, other):
+        try:
+            return self._total_months >= other._total_months
+        except AttributeError:
+            raise TypeError("unsupported operand type(s) for >=: '%s' and '%s'" %
+                            (type(self).__name__, type(other).__name__))
+
+    def __lt__(self, other):
+        try:
+            return self._total_months < other._total_months
+        except AttributeError:
+            raise TypeError("unsupported operand type(s) for <: '%s' and '%s'" %
+                            (type(self).__name__, type(other).__name__))
+
+    def __le__(self, other):
+        try:
+            return self._total_months <= other._total_months
+        except AttributeError:
+            raise TypeError("unsupported operand type(s) for <=: '%s' and '%s'" %
+                            (type(self).__name__, type(other).__name__))
+
+    def __abs__(self):
+        return Monthdelta(months=abs(self._total_months))
+
+    def __neg__(self):
+        return Monthdelta(months=-self._total_months)
+
+    def _add(self, other):
+        if isinstance(other, Monthdelta):
+            return Monthdelta(months=self._total_months + other._total_months)
+
+        new_year = other.year + self.years
+        new_month = other.month + self.months
+        if new_month < 0:
+            new_month += 12
+            new_year -= 1
+        new_year += new_month // 12
+        new_month = new_month % 12
+        return other.replace(year=new_year, month=new_month)
+
+    def __add__(self, other):
+        try:
+            return self._add(other)
+        except AttributeError:
+            raise TypeError("unsupported operand type(s) for +: '%s' and '%s'" %
+                            (type(self).__name__, type(other).__name__))
+
+    def __radd__(self, other):
+        try:
+            return self._add(other)
+        except AttributeError:
+            raise TypeError("unsupported operand type(s) for +: '%s' and '%s'" %
+                            (type(other).__name__, type(self).__name__))
+
+    def __sub__(self, other):
+        if isinstance(other, Monthdelta):
+            return Monthdelta(months=self._total_months - other._total_months)
+        else:
+            raise TypeError("unsupported operand type(s) for +: '%s' and '%s'" %
+                            (type(other).__name__, type(self).__name__))
+
+    def __rsub__(self, other):
+        try:
+            return (-self)._add(other)
+        except AttributeError:
+            raise TypeError("unsupported operand type(s) for -: '%s' and '%s'" %
+                            (type(other).__name__, type(self).__name__))
---
+++ odps/lib/tests/test_ext_types.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+# Copyright 1999-2017 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pickle
+import sys
+from datetime import datetime
+
+from odps.lib.ext_types import Monthdelta
+from odps.tests.core import TestBase
+
+
+class Test(TestBase):
+    def testMonthdelta(self):
+        ym = lambda md: (md.years, md.months)
+
+        self.assertEqual(Monthdelta(years=2, months=5).total_months(), 29)
+        self.assertEqual(int(Monthdelta(years=2, months=5)), 29)
+        if sys.version_info[0] <= 2:
+            self.assertEqual(long(Monthdelta(years=2, months=5)), 29)
+
+        self.assertEqual(ym(Monthdelta(12)), (1, 0))
+        self.assertEqual(ym(Monthdelta(26)), (2, 2))
+        self.assertEqual(ym(Monthdelta(-11)), (-1, 1))
+
+        self.assertEqual(ym(Monthdelta('29')), (2, 5))
+        self.assertEqual(ym(Monthdelta('-11')), (-1, 1))
+        self.assertEqual(ym(Monthdelta('-1year1month')), (-1, 1))
+        self.assertEqual(ym(Monthdelta('1 year 11 month')), (1, 11))
+        self.assertEqual(ym(Monthdelta('12 years')), (12, 0))
+        self.assertEqual(ym(Monthdelta('11 months')), (0, 11))
+        self.assertEqual(ym(Monthdelta('-11 months')), (-1, 1))
+        self.assertRaises(ValueError, Monthdelta, 't months')
+
+        self.assertEqual(pickle.loads(pickle.dumps(Monthdelta(30, 10))), Monthdelta(30, 10))
+
+        self.assertEqual(str(Monthdelta(0)), '0')
+        self.assertEqual(str(Monthdelta(1, 0)), '1 year')
+        self.assertEqual(str(Monthdelta(years=-5, months=0)), '-5 years')
+        self.assertEqual(str(Monthdelta(years=-1, months=1)), '-1 year 1 month')
+        self.assertEqual(str(Monthdelta(months=1)), '1 month')
+        self.assertEqual(str(Monthdelta(10)), '10 months')
+        self.assertEqual(str(Monthdelta(years=20, months=3)), '20 years 3 months')
+        self.assertEqual(repr(Monthdelta(years=20, months=3)), "Monthdelta('20 years 3 months')")
+
+        self.assertEqual(Monthdelta(20, 11), Monthdelta(19, 23))
+        self.assertNotEqual(Monthdelta(20, 12), Monthdelta(19, 23))
+        self.assertNotEqual(Monthdelta(20, 12), 10)
+        self.assertGreater(Monthdelta(20, 12), Monthdelta(19, 23))
+        self.assertGreaterEqual(Monthdelta(20, 11), Monthdelta(19, 23))
+        self.assertGreaterEqual(Monthdelta(20, 12), Monthdelta(19, 23))
+        self.assertLess(Monthdelta(19, 23), Monthdelta(20, 12))
+        self.assertLessEqual(Monthdelta(19, 23), Monthdelta(20, 11))
+        self.assertLessEqual(Monthdelta(19, 23), Monthdelta(20, 12))
+
+        self.assertRaises(TypeError, lambda: Monthdelta(20, 12) > 'abcd')
+        self.assertRaises(TypeError, lambda: Monthdelta(20, 12) >= 'abcd')
+        self.assertRaises(TypeError, lambda: Monthdelta(20, 12) < 'abcd')
+        self.assertRaises(TypeError, lambda: Monthdelta(20, 12) <= 'abcd')
+
+        self.assertEqual(Monthdelta(-2, 7), -Monthdelta(1, 5))
+        self.assertEqual(abs(Monthdelta(-2, 7)), Monthdelta(1, 5))
+        self.assertEqual(Monthdelta(10, 2) + Monthdelta(5, 11), Monthdelta(16, 1))
+        self.assertEqual(Monthdelta(10, 2) - Monthdelta(5, 11), Monthdelta(4, 3))
+        self.assertEqual(datetime(2012, 3, 5) + Monthdelta(4, 7),
+                         datetime(2016, 10, 5))
+        self.assertEqual(Monthdelta(4, 7) + datetime(2012, 3, 5),
+                         datetime(2016, 10, 5))
+        self.assertEqual(Monthdelta(-2, 7) + datetime(2012, 3, 5),
+                         datetime(2010, 10, 5))
+        self.assertEqual(datetime(2012, 7, 12) - Monthdelta(4, 2),
+                         datetime(2008, 5, 12))
+
+        self.assertRaises(TypeError, lambda: Monthdelta(20, 12) + 'abcd')
+        self.assertRaises(TypeError, lambda: 'abcd' + Monthdelta(20, 12))
+        self.assertRaises(TypeError, lambda: Monthdelta(20, 12) - 'abcd')
+        self.assertRaises(TypeError, lambda: 'abcd' - Monthdelta(20, 12))
+
+        try:
+            import pandas as pd
+            self.assertEqual(pd.Timestamp(2012, 3, 5) + Monthdelta(4, 7),
+                             pd.Timestamp(2016, 10, 5))
+            self.assertEqual(Monthdelta(4, 7) + pd.Timestamp(2012, 3, 5),
+                             pd.Timestamp(2016, 10, 5))
+            self.assertEqual(Monthdelta(-2, 7) + pd.Timestamp(2012, 3, 5),
+                             pd.Timestamp(2010, 10, 5))
+            self.assertEqual(pd.Timestamp(2012, 7, 12) - Monthdelta(4, 2),
+                             pd.Timestamp(2008, 5, 12))
+        except ImportError:
+            pass
