4a34f87d8d3466c8d61814d3b440881b9f196e66
wenjun.swj <wenjun.swj@alibaba-inc.com>
2020-10-30 12:49:33 +0800

    Switch deployment to Github Actions

---
+++ .github/workflows/reload-env.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export UNAME="$(uname | awk '{print tolower($0)}')"
+
+if [[ "$GITHUB_REF" =~ ^"refs/tags/" ]]; then
+  export GITHUB_TAG_REF="$GITHUB_REF"
+  unset CYTHON_TRACE
+  export GIT_TAG=$(echo "$GITHUB_REF" | sed -e "s/refs\/tags\///g")
+fi
+
+if [[ $UNAME == "mingw"* ]] || [[ $UNAME == "msys"* ]]; then
+  export UNAME="windows"
+  CONDA=$(echo "/$CONDA" | sed -e 's/\\/\//g' -e 's/://')
+  export PATH="$CONDA/Library:$CONDA/Library/bin:$CONDA/Scripts:$CONDA:$PATH"
+  export PATH="$CONDA/envs/test/Library:$CONDA/envs/test/Library/bin:$CONDA/envs/test/Scripts:$CONDA/envs/test:$PATH"
+else
+  export CONDA="$HOME/miniconda"
+  export PATH="$HOME/miniconda/envs/test/bin:$HOME/miniconda/bin:$PATH"
+fi
+
+export PYTHON=$(python -c "import sys; print('.'.join(str(v) for v in sys.version_info[:3]))")
+
+function retry {
+  r=0
+  until [ "$r" -ge 5 ]; do
+    $@ && break || true
+    r=$((r+1))
+    sleep 1
+  done
+}
+alias pip="retry pip"
+shopt -s expand_aliases
--- bin/travis/linux-install.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-docker pull $DOCKER_IMAGE
-export PYVER=$PYVER
-docker run --rm -e "PYVER=$PYVER" -v `pwd`:/io $DOCKER_IMAGE /io/bin/travis/linux-wheel.sh
