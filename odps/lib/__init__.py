469343a968c280bb80ad461b66bb3ae04f94d1cd
Wenjun Si <wenjun.swj@alibaba-inc.com>
2024-12-05 13:45:42 +0800

    Releases v0.12.1.1 (#255)

--- odps/lib/__init__.py
+++ odps/lib/__init__.py
@@ -13,3 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# make it compatible to imports on PyODPS between 0.11.4 and 0.11.5.
+import requests  # noqa
